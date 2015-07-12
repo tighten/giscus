0faaa7d1185236948a73a254a4f027ef4b231cfe
Matt Stauffer <matt@tighten.co>
2015-07-12 16:14:12 -0400

    Bit of bootstrap landing page fun.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Giscus</title>
+
+        <link rel="stylesheet" href="/css/app.css">
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+            <div class="row col-md-8 col-md-push-2">
+                <h1 class="text-center">Giscus</h1>
+                <p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lorem erat, luctus at diam sed, dapibus facilisis purus. In laoreet enim nunc, ut pretium arcu scelerisque in. Nunc eu cursus nibh. Etiam pulvinar vulputate libero sed molestie. In condimentum varius faucibus.</p>
+                <p><img src="http://placehold.it/900x300"></p>
+                <p>Pitch txt here lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lorem erat, luctus at diam sed, dapibus facilisis purus.</p>
+                <div class="pricing text-center big">
+                    <p><strong>$3/month</strong><br>for unlimited hourly notifications</p>
+                </div>
+                <div class="text-center">
+                    <a class="btn btn-default btn-lg github-sign-up-button">
+                        Sign Up With Github <i class="fa fa-github"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </body>
