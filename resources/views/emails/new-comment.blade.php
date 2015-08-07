7a5c063e3486455debbd3b9b5b74a30319a1b558
Matt Stauffer <matt@tighten.co>
2015-08-06 22:42:46 -0400

    More

--- resources/views/emails/new-comment.blade.php
+++ resources/views/emails/new-comment.blade.php
@@ -1,10 +1,16 @@
-<p>Hi friend!</p>
+<p>You received a new comment on a Gist, <a href="{{ $gist['html_url'] }}">{{ $gist['description'] != '' ? $gist['description'] : '[No Gist title]' }}</a>.</p>
 
-<p>You received a new comment on a Gist from <a href="{{ $comment['user']['html_url'] }}">{{ '@' . $comment['user']['login'] }}</a>.
+<table>
+<tr>
+<td style="width: 20%; vertical-align: top; text-align: right; padding-right: 1em;">
+    <img src="{{ $comment['user']['avatar_url'] }}" style="width: 100px;"></td>
+</td>
+<td style="width: 80%;">
+    <p><a href="{{ $comment['user']['html_url'] }}" style="font-weight: bold;">{{ '@' . $comment['user']['login'] }}</a></p>
 
-<p>Gist: <a href="{{ $gist['html_url'] }}">{{ $gist['description'] }}</a>.</p>
-<div style="background: #eee; padding: 1em;">
-{!! str_replace("\n", "<br>", $comment['body']) !!}
-</div>
-<p><a href="{{ $comment['user']['html_url'] }}">{{ '@' . $comment['user']['login'] }}</a>
-at <a href="{{ $gist['html_url'] }}#gistcomment-{{ $comment['id'] }}">{{ \Carbon\Carbon::createFromFormat('Y-m-d\TH:i:s\Z', $comment['updated_at'])->format('F, j Y - g:ia') }}</a></p>
+    {!! str_replace("\n", "<br>", $comment['body']) !!}
+
+    <p style="color: #777;"><a href="{{ $gist['html_url'] }}#gistcomment-{{ $comment['id'] }}" style="color: #777;">{{ \Carbon\Carbon::createFromFormat('Y-m-d\TH:i:s\Z', $comment['updated_at'])->format('F j, Y - g:ia') }} GMT</a></p>
+</td>
+</tr>
+</table>
