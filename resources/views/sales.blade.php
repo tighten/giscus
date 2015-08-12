15c98a7297d67b0e20278db8007bae3c9df1e77c
Matt Stauffer <matt@tighten.co>
2015-08-12 14:55:24 -0400

    Add do/tighten logos

--- resources/views/sales.blade.php
+++ resources/views/sales.blade.php
@@ -3,9 +3,13 @@
 @section('content')
     <p>You love Gists. They're powerful, free, and convenient. But you've probably run into Gists' one big downfall: <strong>No notifications when anyone comments.</strong></p>
     <p class="pitch">Giscus provides the comment notification system your Gists deserve.</p>
-    <p>Receive up-to-the-hour notifications on comments on your Gists. No work on your part, free becuase DigitalOcean loves you, cancel any time.</p>
+    <p>Receive up-to-the-hour notifications on comments on your Gists. No work on your part, free because Tighten and DigitalOcean love you, cancel any time.</p>
     <div class="pricing well text-center big">
-        <p><strong>Free!</strong><br>Sponsored by <a href="http://digitalocean.com/">DigitalOcean</a></p>
+        <p>
+            <strong>Free!</strong><br>Sponsored by<br><a href="http://digitalocean.com/"><img src="digitalocean-logo.png" alt="DigitalOcean" style="max-width: 25rem"></a><br>
+            and<br>
+            <a href="http://tighten.co/" style="font-size: 3.5rem">Tighten Co.</a>
+        </p>
     </div>
     <div class="text-center">
         <a href="/auth/github" class="btn btn-default btn-lg github-sign-up-button">
