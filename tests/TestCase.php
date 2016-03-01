7cfb214b9b6f3f8a044e05fc275bf1f1dfe46bf8
Matt Stauffer <matt@tighten.co>
2016-03-01 11:32:40 -0500

    Add TEST_API environment variable to .env.test
    Determines whether or not the system runs tests that hit a real user’s
    API (requiring token)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,14 +16,31 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        if (file_exists(dirname(__DIR__) . '/.env.test')) {
-            Dotenv::load(dirname(__DIR__), '.env.test');
-        }
-
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    protected function checkRequirements()
+    {
+        parent::checkRequirements();
+
+        if (file_exists(dirname(__DIR__) . '/.env.test')) {
+            Dotenv::load(dirname(__DIR__), '.env.test');
+        }
+
+        collect($this->getAnnotations())->each(function ($location) {
+            if (! isset($location['requires'])) {
+                return;
+            }
+
+            if (in_array('ApiTest', $location['requires'])) {
+                if (! env('TEST_API')) {
+                    $this->markTestSkipped('Skipping API tests');
+                }
+            }
+        });
     }
 }
