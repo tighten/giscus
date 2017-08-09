f1b0e2b4760a20a5b5830ecc11a0bafb9d24e037
John T. Bonaccorsi <jtbonaccorsi@gmail.com>
2017-08-09 12:54:54 -0400

    Fix spacing

--- app/User.php
+++ app/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+
     protected $fillable = ['name', 'email', 'github_id', 'avatar', 'token'];
 
     protected $hidden = ['password', 'remember_token'];
