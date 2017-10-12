ea8e736577833a575dc29c57b92c4b7587f462e6
Tu <tucq88@gmail.com>
2017-10-13 01:49:25 +0700

    Apply new design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,29 +6,33 @@
 
         <link rel="stylesheet" href="/css/app.css">
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet" type="text/css">
 
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                @if (Auth::check())
-                <a href="/logout" class="btn btn-default pull-right meta-button">Log out <i class="fa fa-sign-out"></i></a>
-                @else
-                <a href="/auth/github" class="btn btn-default pull-right meta-button"><i class="fa fa-github"></i> Log in with GitHub</a>
-                @endif
-                <h1 class="text-center page-title page-title"><img src="/giscus-logo.png" class="giscus-logo giscus-logo" alt="Giscus"></h1>
-            </div>
-            <div class="row col-md-8 col-md-push-2">
-                @yield('content')
+        <header>
+            <div class="ribbon">&nbsp;</div>
+
+            <div class="container">
+                <nav class="navbar navbar-head">
+                    <a class="navbar-brand" href="#">
+                        <img src="/giscus-logo.svg" class="giscus-logo" alt="Giscus">
+                    </a>
+                    <div class="navbar-buttons">
+                            <a href="/logout" class="btn btn-default">
+                                Log out
+                                <i class="fa fa-sign-out"></i>
+                            </a>
+                    </div>
+                </nav>
             </div>
 
-            <div class="row col-md-8 col-md-push-2 text-center footer">
-                Giscus' source is on GitHub: <a href="https://github.com/tightenco/giscus">tightenco/giscus</a>.<br>
-                By <a href="http://mattstauffer.co/">Matt Stauffer</a>, for a <a href="https://speakerdeck.com/mattstauffer/leveraging-laravel-launching-side-projects-quickly-with-laravel">talk at Laracon 2015</a>, powered by <a href="http://www.laravel.com/">Laravel</a>.<br>
-                Logo by <a href="https://twitter.com/Caneco">Vitor Caneco</a>, with a picture of a <a href="https://en.wikipedia.org/wiki/Discus_(fish)">Discus</a> fish
+        </header>
+        <div class="container">
+            <div class="main-content">
+                @yield('content')
             </div>
         </div>
 
