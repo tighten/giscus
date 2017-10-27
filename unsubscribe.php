e7434784fb22b3940d15b97519e29d946f04ffe8
VIRENDRA SINGAL` <virendrasingal@yahoo.com>
2017-10-27 07:05:57 +0530

    Update unsubscribe.php

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,3 +1,13 @@
 ?
 $link = "unsubscribe.php?id=$user['id']&validation_hash=".md5($user['id'].$SECRET_STRING)
 <a href="<?=$link?>">Unsubscribe</a>
+
+function unsubscribe() {
+
+    $expected = md5( $user['id'] . $SECRET_STRING );
+
+    if( $_GET['validation_hash'] != $expected )
+        throw new Exception("Validation failed.");
+
+    sql("UPDATE users SET wants_newsletter = FALSE WHERE id = " . escape($_GET['id']);
+}
