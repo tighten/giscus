1d33122bb3b7ef96859ff0dd5b5b684a0c9d3091
Matt Stauffer <matt@tighten.co>
2018-08-10 13:23:08 -0400

    Drop DigitalOcean sponsorship for now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,9 @@
             <h1 class="welcome-heading">Giscus provides the comment notification system your Gists deserve.</h1>
             <p>You love Gists. They're powerful, free, and convenient. But you've probably run into Gists' one big
                 downfall: <strong>No notifications when anyone comments.</strong></p>
+
             <p>Receive up-to-the-hour notifications on comments on your Gists. No work on your part, free because
-                Tighten and DigitalOcean love you, cancel any time.</p>
+                Tighten {{--and DigitalOcean love--}} loves you, cancel any time.</p>
             <p>
                 <form method="post" action="/auth/github">
                     {{ csrf_field() }}
@@ -23,11 +24,13 @@
             <div>
                 <h4 class="sponsors-label">Sponsored by</h4>
                 <ul class="list-inline">
+                    {{--
                     <li><a href="http://digitalocean.com/" class="sponsor">
                         <img src="/digital-ocean-logo.svg" alt="DigitalOcean">
                     </a></li>
-                    <li><a href="http://tighten.co/" class="sponsor">
-                        <img src="/tighten-logo.svg" alt="Tighten Co.">
+                    --}}
+                    <li><a href="https://tighten.co/" class="sponsor">
+                        <img src="/tighten-logo.svg" alt="Tighten">
                     </a></li>
                 </ul>
             </div>
