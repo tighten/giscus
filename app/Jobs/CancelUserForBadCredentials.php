57b09c2edabcef22324017481259b74a5a486ea4
Shawn Jones <shawn.greg.jones@gmail.com>
2018-09-14 09:30:36 -0500

    update reference for user and import BrokenGitHubToken class to make test pass

--- app/Jobs/CancelUserForBadCredentials.php
+++ app/Jobs/CancelUserForBadCredentials.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Mail\BrokenGitHubToken;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -35,6 +36,6 @@ class CancelUserForBadCredentials extends Job implements ShouldQueue
 
     private function sendNotificationEmail()
     {
-        Mail::to($user)->send(new BrokenGitHubToken($user));
+        Mail::to($this->user)->send(new BrokenGitHubToken($this->user));
     }
 }
