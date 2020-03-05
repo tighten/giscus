9bbb82319e2b3659d0930928c6ad1c565076f8ee
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-03-05 11:03:44 -0500

    Reimplement #64

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -4,6 +4,7 @@ use App\NotifiedComment;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
+use Tests\app\Gist;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,5 +31,30 @@ $factory->define(NotifiedComment::class, function (Faker\Generator $faker) {
     return [
         'github_id' => factory(User::class)->create()->github_id,
         'github_updated_at' => Carbon::now(),
+    ];
+});
+
+$factory->define(Gist::class, function (Faker\Generator $faker) {
+    return [
+        "url" => $faker->url,
+        "forks_url" => $faker->url,
+        "commits_url" => $faker->url,
+        "id" => str_random(30),
+        "node_id" => str_random(30),
+        "git_pull_url" => $faker->url,
+        "git_push_url" => $faker->url,
+        "html_url" => $faker->url,
+        "files" => [],
+        "public" => true,
+        "created_at" => $faker->time('Y-m-d'),
+        "updated_at" => $faker->time('Y-m-d'),
+        "description" => $faker->word,
+        "comments" => 0,
+        "user" => null,
+        "comments_url" => $faker->url,
+        "owner" => [],
+        "forks" => [],
+        "history" => [],
+        "truncated" => false,
     ];
 });
