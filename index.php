e4bc77319f9121d934db7f47d03a023338d9135c
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-03-05 11:49:13 -0500

    Add exclamation mark to title to match production app

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Giscus</title>
+        <title>Giscus!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="/app.css">
